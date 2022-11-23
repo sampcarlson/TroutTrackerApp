1d1ebf52361ea32237780736aaa6df4368b33a60
Sam Carlson <sam.p.carlson@gmail.com>
2022-11-23 00:15:44 -0700

    fix fish animation timing bug

--- app.R
+++ app.R
@@ -1,7 +1,17 @@
 library(shiny)
 
-
 source("init.R")
+
+#todo:
+# check that getting species info from other data sources -< fgdata species
+# kml w/ links in popups (and select w/ kml)
+# pause button - click on map or fish to pause
+# use DT selectable table
+# change date range to reflect selected fish
+# cold phone gps location bug - snap tolerance - snap to next point instead
+
+#addmovingMarker allows numeric layerId, while removeMarker suggests(requires?) character
+
 
 #user interface
 #depends on server side to provide current database
@@ -10,54 +20,129 @@ secPerDay=.25
 
 
 ui=fluidPage(
-  titlePanel("Silver Creek Fish Tracker Test"),
+  #titlePanel("Silver Creek Fish Tracker"),
   fluidRow(
+    column(width=2,
+           textInput(inputId="userName",
+                     label="User Name",
+                     placeholder = "Enter your last name")
+           
+    ),
     column(width=3,
            selectInput(
-             inputId="fishHID",
+             inputId="selectFish",
              label="fish",
              choices=availableFish$l4hex,
              selected=NULL,
              multiple = TRUE
-           ),
-           offset=1
+           )
     ),
-    column(width=5,
-           sliderInput(inputId = "dateTime",
-                       label = "Date Range:",
-                       min = minDateTime,
-                       max = maxDateTime,
-                       value = c(minDateTime,maxDateTime),
-                       step=1
-                       #animate = animationOptions(interval=msPerDay*daysPerStep)
-           ),
-    ),
-    column(width=1,
-           actionButton("startFish","Start"),
-    ),
-    column(width=2,
-           textOutput("simTime"),
+    column(width=3,
+           dateRangeInput(inputId = 'dateTime',
+                          label = "Date Range",
+                          start = minDateTime,
+                          end = maxDateTime,
+                          min = minDateTime,
+                          max = maxDateTime,
+                          startview = "year",
+                          separator = "to ",
+                          autoclose = T
+           )
            
-    )
+           # sliderInput(inputId = "dateTime",
+           #             label = "Date Range:",
+           #             min = minDateTime,
+           #             max = maxDateTime,
+           #             value = c(minDateTime,maxDateTime),
+           #             step=1
+           #             #animate = animationOptions(interval=msPerDay*daysPerStep)
+           # ),
+    ),
+    column(width=3,
+           actionButton("startFish","Start",style="position: relative; top: 22px; right: 20px;"),
+           div(style="position: relative; top: -2px; left: 50px;",
+               textOutput("simTime"),
+           ) 
+           
+    ),
+    # column(width=2,
+    #        div(style="position: relative; top: 25px; right: 30px;",
+    #            textOutput("simTime"),
+    #        )     
+    # )
+  ),
+  fluidRow(
+    column(3,textOutput("nameReport")),
+    column(4,checkboxInput("temperatureLoggers","Display Temperature Loggers"))
   ),
   
   fluidRow(
     column(12,align="center",
-           leafletOutput("scMap",width="100%",height=450)
+           leafletOutput("scMap",width="100%",height=500)
+    )
+  ),
+  fluidRow(
+    column(12,
+           div(style="height:300px; overflow: scroll",
+               tableOutput("availableFishTable")
+           )
     )
   )
 )
-
 
 #server side - update, manage, backup, and serve database
 
 server = function(input,output,session){
   source("init.R")
   #readRenviron(".Renviron")
-  network=readRDS("network.rds")
+  #network=readRDS("network.rds")
+  
+  #availableFish=reactive({ getAvailableFish(input$userName) })
+  
+  observeEvent(input$userName,{
+    
+    
+    name=tolower(input$userName)
+    if(name %in% c(unique(clientFish$Client),"","superuser","contest")){
+      validName=T
+      availableFish <<- getAvailableFish(name)
+      if(name == ""){
+        output$nameReport=renderText("")
+      } else {
+        output$nameReport=renderText(paste(length(unique(availableFish$idx)),"fish found"))
+      } 
+    } else {
+      validName=F
+      output$nameReport=renderText("Name not recognized")
+      availableFish <<- getAvailableFish("")
+    }
+    printFish=availableFish[,c("l4hex","firstObserved","lastObserved","obsCount","Species","Length.Inches","routeLength_km")]
+    names(printFish)=c("Fish ID","First Observed", "Last Observed", "# of Observations", "Species", "Length (in)", "Travel Distance (km)")
+    printFish$`# of Observations`=as.character( printFish$`# of Observations` )
+    printFish$`Length (in)` = as.character(printFish$`Length (in)`)
+    printFish$`Travel Distance (km)`=round(printFish$`Travel Distance (km)`,1)
+    
+    output$availableFishTable=renderTable({printFish},sanitize.text.function=function(x){x})
+    
+    updateSelectInput(inputId="selectFish",choices=unique(availableFish$l4hex))
+    #print(unique(availableFish$l4hex))
+    print(paste("# of availFish:",nrow(availableFish)))
+    
+    
+  })
+  
+  observeEvent(input$selectFish,{
+    
+    ############# ne workez pas -----------
+    minFishDate=fishDetails$firstObserved[fishDetails$hexID %in% input$selectFish]
+    maxFishDate=fishDetails$lastObserved[fishDetails$hexID %in% input$selectFish]
+    updateDateRangeInput(inputId="dateTime",
+                         min=minFishDate,
+                         max=maxFishDate)
+  })
   
   #fishData=reactive(getFishData_interval(input$dateTime,daysPerStep,whichFish=input$fishID))
-  #fishData=reactive(getFishData(input$dateTime[1],input$dateTime[2],fishHIDs=input$fishHID))
+  #fishData=reactive(getFishData(input$dateTime[1],input$dateTime[2],selectFishs=input$selectFish))
   
   
   #clockStartTime = reactiveVal(Sys.time())
@@ -65,21 +150,30 @@ server = function(input,output,session){
   
   
   #https://trackatrout.com/Images/TroutIcon.png
-  fishIcon=makeIcon(iconUrl="https://trackatrout.com/Images/TroutIcon.png",
-                    iconWidth = 60, iconHeight = 60,
-                    iconAnchorX=30,iconAnchorY = 30)
   
   
   scMap = leaflet( leafletOptions(leafletCRS(crsClass="L.CRS.EPSG4326")) )
   
   scMap = setView(map=scMap,lng=-114.15,lat=43.33,zoom=12)
   
-  scMap = addWMSTiles(map=scMap,baseUrl="https://basemap.nationalmap.gov/arcgis/services/USGSImageryOnly/MapServer/WmsServer",
-                      layers=0,
-                      attribution = 'Tiles courtesy of the <a href="https://usgs.gov/">U.S. Geological Survey</a>',
-                      tileOptions(zIndex=1))
+  # scMap = addWMSTiles(map=scMap,baseUrl="https://basemap.nationalmap.gov/arcgis/services/USGSImageryOnly/MapServer/WmsServer",
+  #                     layers=0,
+  #                     attribution = 'Tiles courtesy of the <a href="https://usgs.gov/">U.S. Geological Survey</a>',
+  #                     tileOptions(zIndex=1)) #can set maxZoom in tileOptions
   
-  scMap = addPolylines(map=scMap,opacity=1,data=network,weight=1,smoothFactor = 2,label=network$GNIS_Name)
+  scMap=addProviderTiles(map=scMap,provider = providers$Esri.WorldImagery)
+  
+  #scMap = addPolylines(map=scMap,opacity=1,data=network,weight=1,smoothFactor = 2,label=network$GNIS_Name)
+  
+  # scMap = addMarkers(map=scMap,lng=st_coordinates(lng=add_loggers[,"X"]),lat= )
+  # scMap=addAwesomeMarkers(map=scMap,group="temperatureMarkers",
+  #                         data=add_loggers,
+  #                         icon=loggerIcon,
+  #                         #popup="testPopup",
+  #                         popup=as.character(add_loggers$Description)
+  #                         #options=markerOptions(zIntexOffset=0),
+  #                         #clusterOptions = markerClusterOptions()
+  #                         )
   
   output$scMap = renderLeaflet(scMap)
   
@@ -87,47 +181,100 @@ server = function(input,output,session){
   observeEvent(input$startFish,{
     
     #observe({
-    clockStartTime <<- Sys.time()
+    # clockStartTime <<- Sys.time()
     
-    fishData=getFishData(input$dateTime[1],input$dateTime[2],fishHIDs=input$fishHID)
-    print(fishData)
-    showFish=as.character(unique(fishData$fishid))
+    fishData <<- getFishData(input$dateTime[1],input$dateTime[2],fishHIDs=input$selectFish)
+    
+    addRemoveFishKey <<- fishDetails[fishDetails$idx %in% unique(fishData$fishid),c("idx","l4hex","firstObserved","lastObserved","Species","Length.Inches")]
+    addRemoveFishKey$firstDay <<- as.Date(addRemoveFishKey$firstObserved)
+    addRemoveFishKey$lastDay <<- as.Date(addRemoveFishKey$lastObserved)
+    addRemoveFishKey$firstObserved <<- NULL
+    addRemoveFishKey$lastObserved <<- NULL
+    if(nrow(addRemoveFishKey>=1)){
+      addRemoveFishKey$days <<- mapply(FUN=getFishDuration,
+                                       fishMin=addRemoveFishKey$firstDay,
+                                       fishMax=addRemoveFishKey$lastDay,
+                                       calMin=input$dateTime[1],
+                                       calMax=input$dateTime[2])
+    } else {addRemoveFishKey$days=numeric(0)}
+    # 
     scMap= leafletProxy("scMap",deferUntilFlush = T)
     scMap = clearGroup(map=scMap,"activeFish")
-    days=input$dateTime[2]-input$dateTime[1]
     
-    for(fish in showFish){
+    alreadyPresentFish=addRemoveFishKey[addRemoveFishKey$firstDay<input$dateTime[1] & addRemoveFishKey$lastDay>input$dateTime[1],]
+    
+    for(fish in alreadyPresentFish$idx){
       scMap = addMovingMarker(map=scMap,data=fishData[fishData$fishid==fish,],
-                              layerId=showFish,
+                              layerId=as.character(fish),
                               group="activeFish",
                               icon=fishIcon,
                               label=getFishLabel(fish),
-                              duration=(secPerDay*1000)*days,
-                              movingOptions = movingMarkerOptions(autostart = T, loop = FALSE)
-      ) 
+                              duration=(secPerDay*1000)*alreadyPresentFish$days[alreadyPresentFish$idx==fish],
+                              movingOptions = movingMarkerOptions(autostart = T, loop = FALSE),
+                              options = markerOptions(zIndexOffset = 1000)
+      )
     }
+    
+    clockStartTime <<- Sys.time()
     
   })
   
   observe({
-    #req(clockStartTime)
-    #req(input$dateTime)
-    
-    #Sys.sleep(1)
-    
     simTime=reactive(getSimTime(clockStartTime,simStartTime=input$dateTime[1],simEndTime=input$dateTime[2], secPerDay = secPerDay))
     
     #thisSimTime=getSimTime(clockStartTime,simStartTime=input$dateTime[1],simEndTime=input$dateTime[2], secPerDay = secPerDay)
     
-    invalidateLater(500)
+    invalidateLater(250)
+    
     
     thisSimTime=simTime()
-    #print(thisSimTime)
     output$simTime=renderText({thisSimTime})
     
     
+    if(thisSimTime %in% as.character(addRemoveFishKey$firstDay)){
+      
+      thisAddFish=addRemoveFishKey[as.character(addRemoveFishKey$firstDay)==thisSimTime,]
+      scMap= leafletProxy("scMap",deferUntilFlush = T)
+      
+      for(fish in thisAddFish$idx){
+        scMap = addMovingMarker(map=scMap,data=fishData[fishData$fishid==fish,],
+                                layerId=as.character(fish),
+                                group="activeFish",
+                                icon=fishIcon,
+                                label=getFishLabel(fish),
+                                duration=(secPerDay*1000)*thisAddFish$days[thisAddFish$idx==fish],
+                                movingOptions = movingMarkerOptions(autostart = T, loop = FALSE),
+                                options = markerOptions(zIndexOffset = 1000)
+        ) 
+      }
+    }
     
+    #end of a fish movement
+    if(thisSimTime %in% as.character(addRemoveFishKey$lastDay)){
+      
+      thisRemoveFish=addRemoveFishKey[as.character(addRemoveFishKey$lastDay)==thisSimTime,]
+      scMap= leafletProxy("scMap",deferUntilFlush = T)
+      
+      removeFishData=fishData[order(fishData$datetime,decreasing = T),]
+      removeFishData=removeFishData[!duplicated(removeFishData$fishid),]
+      removeFishData=removeFishData[removeFishData$fishid %in% thisRemoveFish$idx,]
+      
+      removeMarker(map=scMap,layerId = as.character(removeFishData$fishid))
+      
+      addMarkers(map=scMap,
+                 data=removeFishData,
+                 layerId=removeFishData$fishid,
+                 group="activeFish",
+                 icon=fishIcon,
+                 popup=getFishLabel(removeFishData$fishid),
+                 options=markerOptions(opacity=.65))
+      
+      
+      
+    }
   })
-}
+  
+  
+} 
 # Run the application 
 shinyApp(ui = ui, server = server)
--- init.R
+++ init.R
@@ -9,8 +9,7 @@ library(DBI)
 
 source("movingMarkerFunctions.R")
 
-userRole="user"
-#userRole="superuser"
+validName=T
 
 scdbConnect=function(){
   #readRenviron(".Renviron")
@@ -28,10 +27,7 @@ scdbConnect=function(){
 
 conn=scdbConnect()
 
-#write temp data to fishlocations
-#fishData=st_read("fishLocationTimeseries.gpkg")
-#writeMe=data.frame(fishid=fishData$fishID,datetime=fishData$time,geometry=fishData$geom)
-#st_write(writeMe,dsn=conn,"fishlocations")
+
 
 minDateTime=as.Date(dbGetQuery(conn,"SELECT MIN (datetime) FROM fishlocations;")$min)
 maxDateTime=as.Date(dbGetQuery(conn,"SELECT MAX (datetime) FROM fishlocations;")$max)
@@ -42,14 +38,17 @@ maxDateTime=as.Date(dbGetQuery(conn,"SELECT MAX (datetime) FROM fishlocations;")
 
 
 getFishData=function(startDateTime,stopDateTime,fishIDs=NULL,fishHIDs=NULL,availFish=availableFish){
-  print(fishIDs)
-  print(fishHIDs)
+  #print(fishIDs)
+  #print(fishHIDs)
+  #print("fish in availFish in getFishData:")
+  #print(paste(unique(availFish$l4hex)))
   if(is.null(fishIDs) & !is.null(fishHIDs)){
     fishIDs=availFish[availFish$l4hex %in% fishHIDs,]
+    #print(fishIDs)
   }
   
   whichFish = fishIDs[fishIDs$idx %in% availFish$idx,"idx"]
-  print(whichFish)
+  #print(whichFish)
   
   if(length(whichFish)>0){
     q=paste0("SELECT * FROM fishlocations WHERE fishlocations.datetime > '",startDateTime,
@@ -62,17 +61,17 @@ getFishData=function(startDateTime,stopDateTime,fishIDs=NULL,fishHIDs=NULL,avail
   
 }
 
-getFishData_interval=function(startDateTime,intervalDays,whichFish){
-  if(length(whichFish)>0){
-    stopDateTime=startDateTime+intervalDays
-    q=paste0("SELECT * FROM fishlocations WHERE fishlocations.datetime > '",startDateTime,
-             "' AND fishlocations.datetime < '",stopDateTime,
-             "' AND fishlocations.fishid IN ('",paste0(whichFish,collapse="', '"),"');")
-    fishData=st_read(dsn=conn,query=q)
-    return(fishData)
-  } else { return(NULL) }
-  
-}
+# getFishData_interval=function(startDateTime,intervalDays,whichFish){
+#   if(length(whichFish)>0){
+#     stopDateTime=startDateTime+intervalDays
+#     q=paste0("SELECT * FROM fishlocations WHERE fishlocations.datetime > '",startDateTime,
+#              "' AND fishlocations.datetime < '",stopDateTime,
+#              "' AND fishlocations.fishid IN ('",paste0(whichFish,collapse="', '"),"');")
+#     fishData=st_read(dsn=conn,query=q)
+#     return(fishData)
+#   } else { return(NULL) }
+#   
+# }
 
 dbGetQuery(conn,"SELECT * FROM fishlocations LIMIT 5;")
 
@@ -110,13 +109,87 @@ getFishLabel=function(fishid,fishDeets=fishDetails){
   
 }
 
-getAvailableFish=function(role="user",fishDeets=fishDetails){
-  if(role=="superuser"){
-    return(fishDeets[,c("idx","hexID","l4hex")])
-  }else {
-    return(fishDeets[1:10,c("idx","hexID","l4hex")])
-  }
+make4bold=function(string){
+  n=nchar(string)
+  firstbit=substring(string,first=1,last=n-4)
+  lastbit=substring(string,first=n-3)
+  boldString=paste0(firstbit,"<strong>",lastbit,"</strong>")
+  return(boldString)
 }
-availableFish=getAvailableFish(role=userRole)
+
+getAvailableFish=function(name="",fishDeets=fishDetails){
+  fishDeets=fishDeets[,c("idx","hexID","l4hex","firstObserved","lastObserved","obsCount","Species","Length.Inches","routeLength_km")]
+
+    if(name=="superuser"){
+      outdf=fishDeets
+    } else if(name %in% unique(clientFish$Client)){
+      fishIDs=clientFish$idx[clientFish$Client==name]
+      #print(fishIDs)
+      outdf=fishDeets[fishDeets$idx %in% fishIDs,]
+    } else {
+      #fishDeets=fishDeets[order(fishDeets$lastObserved,decreasing=T),]
+      outdf=fishDeets[1:5,]
+    }
+    outdf$firstObserved=as.character(as.Date(outdf$firstObserved))
+    outdf$lastObserved=as.character(as.Date(outdf$lastObserved))
+    outdf$hexBold=make4bold(outdf$hexID)
+    
+    outdf=outdf[order(outdf$routeLength_km,decreasing = T),]
+    #print(outdf)
+    return(outdf)
+  
+}
+
+getFishDuration=function(fishMin,fishMax,calMin,calMax){
+  days=min(fishMax,calMax)-max(fishMin,calMin)
+  return(days)
+}
+
+clientFish=dbGetQuery(conn,"select * from clientfish;")
+clientFish$ncharClient=nchar(clientFish$Client)
+clientFish=clientFish[clientFish$ncharClient>=1,c("idx","l4hex","Client")]
+clientFish$Client=tolower(clientFish$Client)
+#do_loggers=st_read("C:/Users/sam/Documents/SilverCreek/R/TroutTrackerApp/www/DO_Loggers_2020.kml")
+
+add_loggers = st_read("https://savesilvercreek.org/2020_KML_Files/Temperature%20Data%202020.kml") 
+
+#loggerIcon=makeAwesomeIcon(icon="thermometer",markerColor = "darkblue",library = "fa",iconColor = "black")
+
+fixHtmlLink=function(sfObject){
+  #inputFormat = "<!DOCTYPE html> <html> <body> <strong>Temperature Data for Lower Silver Creek at Highway 93 #1</strong> <br /> <a href=\"https://savesilvercreek.org/data-for-1-highway-93/\">Logger  Data Page</a> <br /> </body> </html>"
+  #output format: 
+  #"<a target=_'blank' href=\"https://savesilvercreek.org/data-for-1-highway-93/\">Logger  Data Page</a> <br />"
+  for(i in 1:nrow(sfObject)){
+    htmlLink=sfObject$Description[i]
+    
+    # linkName=substring(htmlLink,
+    #                    first=regexpr("<strong>",htmlLink)[1]+8,
+    #                    last=regexpr("</strong>",htmlLink)[1])
+    # 
+    htmlLink=htmltools::HTML("<a target='_blank' href=",
+                             substring(htmlLink,
+                                       first=regexpr('"https://',htmlLink)[1],
+                                       last=regexpr('/">',htmlLink)[1]
+                             ),'\"> Test Link Text </a>'
+    )
+    #htmlLink=sub("</body> </html>","",htmlLink)
+    #htmlLink=sub('<img src=""> ',"",htmlLink)
+    
+    sfObject$Description[i]=htmlLink
+  }
+  return(sfObject)
+}
+
+#temperatureLoggers=fixHtmlLink(add_loggers)
+
+
+availableFish=getAvailableFish("")
+addRemoveFishKey = data.frame(idx=118,l4hex="80FB",Species="Brown Trout", Length.Inches=5.5, firstDay=as.Date("2022-04-28"),lastDay=as.Date("2022-05-11"),days=13)
+addRemoveFishKey=addRemoveFishKey[0,]
+
+fishIcon=makeIcon(iconUrl="https://trackatrout.com/Images/TroutIcon.png",
+                  iconWidth = 60, iconHeight = 60,
+                  iconAnchorX=30,iconAnchorY = 30)
+
 
 #rsconnect::deployApp()
